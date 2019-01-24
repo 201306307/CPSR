ea6a38da9787e36eba3afb635286ef8daaac6797
Alonso Gonzalez <201306307@alu.comillas.edu>
2019-01-24 22:08:45 +0100

    arreglar parentesis

--- Lab01/navigation.py
+++ Lab01/navigation.py
@@ -39,7 +39,7 @@ class Navigation:
                 v = 2
         else:
             #CONTROL EN CURVA
-            (if measurements[16-1] > 0.99 and [1-1] > 0.99) or (measurements[8-1] < 0.5 and measurements[9-1] < 0.5):
+            if (measurements[16-1] > 0.99 and [1-1] > 0.99) or (measurements[8-1] < 0.5 and measurements[9-1] < 0.5):
                 w = 0.25
                 v = 0
                 print("curva seguir pared giro hacia izq")
