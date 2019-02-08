c79b92d9f82f733e878deda7cf7e2d1737f9b3c1
Alonso Gonzalez <201306307@alu.comillas.edu>
2019-02-08 13:12:31 +0100

    Support different OS

--- Lab_02/cConst.py
+++ Lab_02/cConst.py
@@ -1,6 +1,19 @@
 import ctypes
+import platform
+import os
 
-intersect_in_c = ctypes.CDLL("cpsr.so")
+file_extension = '.so'
+if platform.system() =='cli':
+    file_extension = '.dll'
+elif platform.system() =='Windows':
+    file_extension = '.dll'
+elif platform.system() == 'Darwin':
+    file_extension = '.so'
+else:
+    file_extension = '.so'
+path = os.path.join(os.path.dirname(__file__), 'cpsr' + file_extension)
+
+intersect_in_c = ctypes.CDLL(path)
 
 intersect_in_c.intersect.argtypes = (ctypes.c_float, ctypes.c_float, ctypes.c_float, ctypes.c_float, ctypes.c_float, ctypes.c_float, ctypes.c_float, ctypes.c_float, ctypes.POINTER(ctypes.ARRAY(ctypes.c_float,2)))
 
